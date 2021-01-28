396bef9c23d65c34af3780431d2db9a4f8bb5a04
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2021-01-28 03:10:14 +0100

    Add basic files

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WebDev News</title>
+        <meta name="keyworkds" content="web development, programming" />
+      </Head>
+      <h1>Welcome to Next</h1>
+    </div>
+  )
+}
