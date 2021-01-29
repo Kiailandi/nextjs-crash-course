8bb26aee7a69bc369710ddc27ecb2f651106a7d3
Tommaso Montefusco <Tommaso.Montefusco@hotmail.it>
2021-01-29 02:37:08 +0100

    Update pages to use new api and add config for dev env

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
+import { server } from '../config';
 import Head from 'next/head';
 import PostList from '../components/PostList';
 
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+//   const posts = await res.json();
+//   return {
+//     props: {
+//       posts
+//     }
+//   }
+// }
+
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`);
+  const res = await fetch(`${server}/api/posts`);
   const posts = await res.json();
 
   return {
--- pages/post/[id]/index.js
+++ pages/post/[id]/index.js
@@ -1,3 +1,4 @@
+import { server } from '../../../config';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -14,7 +15,7 @@ const post = ({ post }) => {
     );
 }
 
-export const getStaticProps = async (context) => {
+/* export const getStaticProps = async (context) => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`);
 
     const post = await res.json();
@@ -28,6 +29,32 @@ export const getStaticProps = async (context) => {
 
 export const getStaticPaths = async () => {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+
+    const posts = await res.json();
+
+    const ids = posts.map(post => post.id);
+    const paths = ids.map(id => ({ params: { id: id.toString() } }));
+
+    return {
+        paths,
+        fallback: false
+    }
+} */
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(`${server}/api/posts/${context.params.id}`);
+
+    const post = await res.json();
+
+    return {
+        props: {
+            post
+        }
+    };
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(`${server}/api/posts`);
 
     const posts = await res.json();
 
